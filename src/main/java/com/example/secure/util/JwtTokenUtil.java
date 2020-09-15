838351c8a9027ff6c87d77a37b4387c78a134851
Daro <decagon@Decagons-MacBook-Air-2.local>
2020-09-15 09:13:36 +0100

    add jwttokenutil to util folder

--- src/main/java/com/example/secure/util/JwtTokenUtil.java
+++ src/main/java/com/example/secure/util/JwtTokenUtil.java
@@ -1,4 +1,35 @@
 package com.example.secure.util;
 
-public class JwtTokenUtil {
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+import java.util.function.Function;
+
+@Component
+public class JwtTokenUtil implements Serializable {
+
+    private static final long serialVersionUID = 4l;
+
+    private static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    public String getUsernameFromToken (String token) {
+        return getClaimFromToken(token, Claims::getSubject);
+    }
+
+    public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+    final Claims claims = getAllClaimsFromToken(token);
+    return claimsResolver.apply(claims);
+    }
+
+    private Claims getAllClaimsFromToken(String token) {
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+    }
+
+
 }
